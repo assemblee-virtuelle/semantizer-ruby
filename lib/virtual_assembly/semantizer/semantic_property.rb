09f54091c68f4544baef9f81d6ce53d806c262a5
Maikel Linke <maikel@email.org.au>
2023-06-05 13:23:04 +1000

    Style SemanticProperty with rubocop

--- lib/virtual_assembly/semantizer/semantic_property.rb
+++ lib/virtual_assembly/semantizer/semantic_property.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # Copyright © 2023 Maxime Lecoq, <maxime@lecoqlibre.fr>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software
@@ -15,15 +17,15 @@
 # DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-# The SemanticPropety class is designed to turn properties of 
+# The SemanticPropety class is designed to turn properties of
 # objects into linked data.
 #
 # A SemanticProperty has a name and a corresponding value that
 # can be fetched later (so its value would be up to date).
 #
-# This class is intented to be used through the SemanticObject 
+# This class is intented to be used through the SemanticObject
 # class.
-# 
+#
 # For instance, we can tell that the name of a Person object refers
 # to the linked data concept "name" from the FOAF project. The name
 # of the property would be the uri of the FOAF:name property while the
@@ -31,33 +33,35 @@
 #
 # You should use a block to pass the value like so:
 # SemanticProperty.new("http://xmlns.com/foaf/0.1/name") {self.name}
-class VirtualAssembly::Semantizer::SemanticProperty
+module VirtualAssembly
+  module Semantizer
+    class SemanticProperty
+      # The name of the property. It generally points to an uri
+      # like "http://xmlns.com/foaf/0.1/name" or it is used to
+      # define a reserved linked data property like "@id".
+      #
+      # This should be a String.
+      attr_accessor :name
 
-    # The name of the property. It generally points to an uri
-    # like "http://xmlns.com/foaf/0.1/name" or it is used to 
-    # define a reserved linked data property like "@id".
-    #
-    # This should be a String.
-    attr_accessor :name
+      # The function to call when the value is requested.
+      #
+      # This should be a Proc passed as a Block.
+      attr_accessor :valueGetter
 
-    # The function to call when the value is requested.
-    #
-    # This should be a Proc passed as a Block.
-    attr_accessor :valueGetter
-
-    # @param name The name of the property, like 
-    # "http://xmlns.com/foaf/0.1/name" or "@id" for instance.
-    #
-    # @param valueGetter A Proc used to retrieve the value of the 
-    # property when requested. 
-    def initialize(name, &valueGetter)
+      # @param name The name of the property, like
+      # "http://xmlns.com/foaf/0.1/name" or "@id" for instance.
+      #
+      # @param valueGetter A Proc used to retrieve the value of the
+      # property when requested.
+      def initialize(name, &valueGetter)
         @name = name
         @valueGetter = valueGetter
-    end
+      end
 
-    # Fetch and returns the value associated to this property.
-    def value
-        return @valueGetter.call
+      # Fetch and returns the value associated to this property.
+      def value
+        @valueGetter.call
+      end
     end
-
+  end
 end
